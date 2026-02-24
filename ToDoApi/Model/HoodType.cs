6c2d9f5cd56879b68e3ec3e89e1c07fc99da5e3d
Vladimir Savitskiy <vladsav33@yandex.ru>
2026-02-24 18:24:41 +1300

    Added Hood description and bin to the hood endpoints

--- ToDoApi/Model/HoodType.cs
+++ ToDoApi/Model/HoodType.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string? ShortName { get; set; }
+        public decimal? HoodBin { get; set; }
+        public string? HoodNote { get; set; }
         public int ItemId { get; set; }
     }
 }
